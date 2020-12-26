623761562f2a3b9d4595aac5254697e600cc0192
Omar <omardraz94@gmail.com>
2020-12-25 22:47:48 -0500

    extra commit

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,9 @@
+import os
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.common.exceptions import WebDriverException
 from selenium.webdriver.common.keys import Keys
 import time
-import os 
 
 MAX_WAIT = 10
 
@@ -14,7 +14,8 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.browser = webdriver.Firefox()
         staging_server = os.environ.get('STAGING_SERVER')
         if staging_server:
-            self.live_server_url = 'http://' + staging_server 
+            self.live_server_url = 'http://' + staging_server
+
 
     def tearDown(self):
         self.browser.quit()
@@ -32,6 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
                 if time.time() - start_time > MAX_WAIT:
                     raise e
                 time.sleep(0.5)
+
 
     def test_can_start_a_list_for_one_user(self):
         # Edith has heard about a cool new online to-do app. She goes
@@ -117,18 +119,22 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-    
+
+
     def test_layout_and_styling(self):
+        # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
+        # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
             delta=10
         )
-         # She starts a new list and sees the input is nicely
+
+        # She starts a new list and sees the input is nicely
         # centered there too
         inputbox.send_keys('testing')
         inputbox.send_keys(Keys.ENTER)
